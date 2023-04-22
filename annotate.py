de885f5a3b79607dbb4b4a00e8f6d20c4946a7df
LTCharlton <linser22@gmail.com>
2023-04-22 03:08:30 -0700

    using 'drawing' widget instead of 'image' widget

--- annotate.py
+++ annotate.py
@@ -1,16 +1,23 @@
 # import ----------------------
 
-from guizero import  App, Picture
+from guizero import  App, Drawing, Text
 
-# funmctions ------------------
+# functions ------------------
 
+def draw_image():
+    picture.clear()
+    picture.image(0,0,"image.png")
+    picture.text(20, 100, inside_test.value)
 
 # variables -------------------
 
 # app -------------------------
 
 app = App("Annotate Tool")
-Picture = Picture(app, image="image.png")
+text = Text(app, text="Annotate below")
+picture = Drawing(app, width="fill", height="fill")
+inside_test = Text(app, text="test")
 
+draw_image()
 
 app.display()
