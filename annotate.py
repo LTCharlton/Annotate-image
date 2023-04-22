255e9225be1b56904692081dbd0737949c95c904
LTCharlton <linser22@gmail.com>
2023-04-22 02:36:29 -0700

    it starts

---
+++ annotate.py
@@ -0,0 +1,16 @@
+# import ----------------------
+
+from guizero import  App, Picture
+
+# funmctions ------------------
+
+
+# variables -------------------
+
+# app -------------------------
+
+app = App("Annotate Tool")
+Picture = Picture(app, image="image.png")
+
+
+app.display()
